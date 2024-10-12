8346a14b4002eeaf3eb7ac68337c34cd7a90b27b
wizzthe3rd <wesleyosayande@gmail.com>
2024-10-12 14:45:48 +0100

    merged footer with map component

--- my-new-app/App.js
+++ my-new-app/App.js
@@ -4,25 +4,14 @@ import { createStackNavigator } from '@react-navigation/stack';
 import Card from './components/Card.js';
 import Footer from './components/Footer.js';
 import SearchBar from './components/Searchbar.js';
-
-const Stack = createStackNavigator();
-
-const handleSearch = (query) => {
-  if (query) {
-    const newData = data.filter(item => item.toLowerCase().includes(query.toLowerCase()));
-    setFilteredData(newData);
-  } else {
-    setFilteredData(data);
-  }
-};
-
+import Map from './components/Map.js';
 
 export default function App() {
   
   return (
     <View style={styles.container}>
       <ScrollView scrollEnabled={false} style={styles.content}>
-        <Card />
+        <Map />
       </ScrollView>
       <Footer style={styles.Footer}/>
     </View>

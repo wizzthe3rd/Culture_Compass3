ad72db7f36cb3bf7f8d45715992ba060c7f5515e
Ignatius <ignatiusboateng123@gmail.com>
2024-10-13 22:07:06 +0100

    server push

---
+++ server/run.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# This script activates the environment and runs the project
+
+# Activate the virtual environment
+source venv/Scripts/activate
+
+# Run your Python application or any commands to start the project
+python app.py

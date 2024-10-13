08f2f1b0334fc37a0b00feba0b10a15f97976abf
Ignatius <ignatiusboateng123@gmail.com>
2024-10-14 00:57:47 +0100

    insertLocationsIntoDatabase control flag

--- client/my-new-app/components/Map.js
+++ client/my-new-app/components/Map.js
@@ -7,6 +7,7 @@ import cities from '../utils/cities'
 import { customDarkThemeMapStyle } from '../utils/mapUtils'
 import {SERVER_API_URL, MAPS_API_KEY, GEMINI_API_KEY} from "@env"
 
+const insertLocationsIntoDatabase = false;
 const windowWidth = Dimensions.get('window').width;
 const windowHeight = Dimensions.get('window').height;
 
@@ -104,16 +105,20 @@ export default function Map({ refocus }) {
             description: geminiRes, // Store Gemini response as the description
           };
           // IF SERVER reset_locations_on_start flag is True, then locations databse will be empty before this code block, be careful
-          /* use the one from map utils, this is depreciated
-          try {
-            const locationResponse = await axios.post(`${SERVER_API_URL}/locations`, singleton); 
-            // console.log(`${SERVER_API_URL}`) // Await the response
-            console.log(`Singleton : ${JSON.stringify(singleton, null, 2)}`)
-            console.log("Singleton was sent to the server \n")
-          } catch (error) {
-            throw error // null
-          }     
-          */   
+          
+           // use the one from map utils, this is depreciated
+
+          
+           if (insertLocationsIntoDatabase) {
+            try {
+              const locationResponse = await axios.post(`${SERVER_API_URL}/locations`, singleton); 
+              // console.log(`${SERVER_API_URL}`) // Await the response
+              console.log(`Singleton : ${JSON.stringify(singleton, null, 2)}`)
+              console.log("Singleton was sent to the server \n")
+            } catch (error) {
+              throw error // null
+            }     
+          }
           return singleton;
         }));
         
